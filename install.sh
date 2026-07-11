74283187c9d3f4a6ca931dcce62fd91ef4d6cd4b
spottyunderdog <spottyunderdog18@gmail.com>
2026-07-10 20:55:47 -0400

    Added Log Files

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 local_dir=$(pwd)
+# Creates a Log File Directory in the current working directory to store log files for the installation process
+mkdir -p "$local_dir/Log Files"
 
 echo "Welcome to Spotty's KDE Theme Installer"
 
@@ -12,6 +14,8 @@ sudo pacman -Syu >> $local_dir/Log\ Files/pacman.log
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
 
+echo "System Updated Successfully"
+
 echo "==Installing Oxygen Themes=="
 
 # Installing the Oxygen theme and related packages for KDE Plasma
@@ -19,14 +23,17 @@ sudo pacman -S --needed --noconfirm oxygen oxygen-cursors oxygen-icons oxygen-ic
 # Add seperators to the log file to make it easier to read
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
+echo "Oxygen Themes Installed Successfully"
+
 
 echo "==Installing Papirus Icons==" 
 
 # Using the official Papirus install script to install the latest version of the Papirus icon theme
 # There is a pacman package for Papirus, But i'd rather use the official script to ensure I get the latest version of the theme.
 wget -qO- https://git.io/papirus-icon-theme-install | sh >> $local_dir/Log\ Files/papiruse_install.log
+echo "Papirus Icons Installed Successfully"
 
 # Runs the manual install script for JetBrains Mono font to ensure the latest version is installed.
 echo "==Installing JetBrains Mono Font=="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)" >> $local_dir/Log\ Files/jetbrains_mono_install.log
-
+echo "Jet Brains Mono Font Installed Successfully"
