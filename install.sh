c055cc395676db9067a3cd580cc7348ada641599
spottyunderdog <spottyunderdog18@gmail.com>
2026-07-10 21:05:59 -0400

    Refactor install.sh to check for log directory existence and update logging syntax

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 local_dir=$(pwd)
-# Creates a Log File Directory in the current working directory to store log files for the installation process
-mkdir -p "$local_dir/Log Files"
+
+# Probably redudent, But if the Log file directory doesn't exist, creates it
+if [ ! -d "$local_dir/Log Files" ]
+then
+    echo "Log Directory does not exist, creating."
+    mkdir -p "$local_dir/Log Files"
+fi
 
 echo "Welcome to Spotty's KDE Theme Installer"
 
 echo "==Updating System=="
 
 # Updating the system to ensure all packages are up to date before installing new themes
-sudo pacman -Syu >> $local_dir/Log\ Files/pacman.log
+sudo pacman -Syu &>> $local_dir/Log\ Files/pacman.log
 # Add seperators to the log file to make it easier to read
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
@@ -19,7 +24,7 @@ echo "System Updated Successfully"
 echo "==Installing Oxygen Themes=="
 
 # Installing the Oxygen theme and related packages for KDE Plasma
-sudo pacman -S --needed --noconfirm oxygen oxygen-cursors oxygen-icons oxygen-icons-svg oxygen-sounds oxygen5 >> $local_dir/Log\ Files/pacman.log
+sudo pacman -S --needed --noconfirm oxygen oxygen-cursors oxygen-icons oxygen-icons-svg oxygen-sounds oxygen5 &>> $local_dir/Log\ Files/pacman.log
 # Add seperators to the log file to make it easier to read
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
 echo "=====================================" >> $local_dir/Log\ Files/pacman.log
@@ -30,10 +35,10 @@ echo "==Installing Papirus Icons=="
 
 # Using the official Papirus install script to install the latest version of the Papirus icon theme
 # There is a pacman package for Papirus, But i'd rather use the official script to ensure I get the latest version of the theme.
-wget -qO- https://git.io/papirus-icon-theme-install | sh >> $local_dir/Log\ Files/papiruse_install.log
+wget -qO- https://git.io/papirus-icon-theme-install | sh &>> $local_dir/Log\ Files/papiruse_install.log
 echo "Papirus Icons Installed Successfully"
 
 # Runs the manual install script for JetBrains Mono font to ensure the latest version is installed.
 echo "==Installing JetBrains Mono Font=="
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)" >> $local_dir/Log\ Files/jetbrains_mono_install.log
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)" &>> $local_dir/Log\ Files/jetbrains_mono_install.log
 echo "Jet Brains Mono Font Installed Successfully"
