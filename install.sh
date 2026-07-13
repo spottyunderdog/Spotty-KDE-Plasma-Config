ed756188cf3161089bfb04a9b5dfdf33c76f33c3
spottyunderdog <spottyunderdog18@gmail.com>
2026-07-13 14:11:27 -0400

    Added Plymouth Themes and Burn My Windows Install Scripts

--- install.sh
+++ install.sh
@@ -44,4 +44,40 @@ echo "==Installing JetBrains Mono Font=="
 echo "Jet Brains Mono Font Installed Successfully"
 
 echo "==Installing Plymouth Boot Themes=="
-git clone https://github.com/adi1090x/plymouth-themes
+git clone https://github.com/adi1090x/plymouth-themes &>> $local_dir/Log\ Files/plymouth_themes.log
+# Removes the included template them from the cloned repo, as it is unneeded
+rm -rf $local_dir/plymouth-themes/template
+
+# Installs the Plymouth themes to the system. IE Coppies each included theme to the /usr/share/plymouth/themes directory
+
+for themepack in $local_dir/plymouth-themes/*; do
+
+    if [ -d $themepack ]
+    then
+        sudo cp -r $themepack/* /usr/share/plymouth/themes/
+    fi
+done
+
+# Removes the cloned repo after the themes have been installed to the system
+rm -rf $local_dir/plymouth-themes
+
+echo "Plymouth Boot Themes Installed Successfully"
+
+echo "==Installing Window Title Widget=="
+
+git clone https://github.com/harunkrl/plasma6-window-title-applet &>> $local_dir/Log\ Files/plasma6_window_title_applet.log
+
+sudo mkdir -p /usr/share/plasma/plasmoids/org.kde.windowtitle
+sudo cp -r $local_dir/plasma6-window-title-applet/* /usr/share/plasma/plasmoids/org.kde.windowtitle/
+rm -rf $local_dir/plasma6-window-title-applet
+echo "Window Title Widget Installed Successfully"
+
+echo "==Installing Burn My Windows Desktop Effects=="
+wget https://github.com/Schneegans/Burn-My-Windows/releases/latest/download/burn_my_windows_kwin6.tar.gz &>> $local_dir/Log\ Files/burn_my_windows.log
+
+sudo mkdir -p /usr/share/kwin/effects
+sudo tar -xf burn_my_windows_kwin6.tar.gz -C /usr/share/kwin/effects
+echo "Burn My Windows Desktop Effects Installed Successfully"
+
+echo "Restarting Plasma to apply changes"
+systemctl --user restart plasma-plasmashell
