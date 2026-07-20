c38c89f5aa5a4e71312c8e2b760fa003a82499b0
spottyunderdog <spottyunderdog18@gmail.com>
2026-07-20 06:31:28 -0400

    Install Splash Screens Function completed.

--- Scripts/functions.sh
+++ Scripts/functions.sh
@@ -24,6 +24,15 @@ isGlobalTheme() {
     false
 }
 
+isInUsrShareDir() {
+    local path=$1
+
+    case $path in
+        /usr/share/*) true; return;;
+    esac
+    false
+}
+
 isUserInstall() {
     local option=$1
     if [[ "$option" == "--user" || "$option" == "-u" ]]
@@ -86,7 +95,7 @@ installJetbrains() {
     
     else
         local exitCode=141
-        echo "Invalid Option or No option Provided"
+        echo "Invalid option or no option provided in function call"
         echo "Exit Code: $exitCode"
         exit $exitCode
     fi
@@ -121,101 +130,132 @@ installSpottyKDE() {
     fi
 }
 
-
 installSplashScreens() {
+
+    addSplashsToDirectory(){
+        local themePath=$1
+        local installDir=$2
+        echo "Splash Theme List"
+            printValidSplashThemes $themePath
+            local numThemes=$(numValidSplashThemes "$themePath")
+            echo ""
+            echo "Enter the theme to be installed alongside SpottyKDE."
+            read -rp "Enter num: (1-$numThemes) " themeSelection
+            while ! isValidInputNum $themeSelection 1 $numThemes
+            do
+                read -rp "Enter num: (1-$numThemes) " themeSelection
+            done
+            local selectedTheme="$(findSplashTheme $themeSelection $themePath)"
+
+            if isInUsrShareDir  "$installDir"
+            then
+                sudo cp -r $themePath/$selectedTheme/contents/* "$installDir"
+            else
+                cp -r $themePath/$selectedTheme/contents/* "$installDir"
+            fi
+
+            read -rp "Would you like to install all Splash Screens as a Global theme? (Y/n) " addAllSplashes
+            if [[ "$addAllSplashes" =~ ^[Yy] ]]
+            then
+                addSplashThemesAsGlobalThemes $localDir
+            fi
+    }
+    addSplashThemesAsGlobalThemes() {
+        local localDir=$1
+        local themeList
+        themeList=$(ls -d "$localDir"/themes/KDE-loginscreens/*/)
+        local theme
+        echo "Adding All Splash Screens As Global Themes"
+        for theme in $themeList
+        do 
+            if isGlobalTheme $theme
+            then
+                echo "Adding $(basename $theme)"
+                cp -r $theme "$HOME"/.local/share/plasma/look-and-feel
+            fi
+        done
+        echo "Splashes Added"
+    }
+    findSplashTheme() {
+        local num=$1
+        local themesDir=$2
+        local themeList
+        themeList=$(ls -d "$themesDir"/*/)
+        local count=1
+
+        for theme in $themeList
+        do
+
+            if isGlobalTheme $theme && [ $count -eq "$num" ]
+            then
+                echo "$(basename $theme)"
+                break
+            elif isGlobalTheme $theme
+            then
+                ((count++))
+            fi
+
+        done
+    }
+    numValidSplashThemes() {
+        local themesDir=$1
+        local themeList
+        themeList=$(ls -d "$themesDir"/*/)
+        local count=1
+        local theme
+
+        for theme in $themeList
+        do
+            
+            if isGlobalTheme $theme
+            then
+                ((count++))
+            fi
+        done
+        echo $((count-1))
+    }
+    printValidSplashThemes() {
+        local themesDir=$1
+        local themeList
+        themeList=$(ls -d "$themesDir"/*/)
+        local count=1
+        local theme
+
+        for theme in $themeList
+        do
+            if isGlobalTheme "$theme"
+            then
+                echo "$count: $(basename "$theme")"
+                ((count++))
+            fi
+        done
+    }
     local localDir=$1
     local installType=$2
     themePath=$localDir/themes/KDE-loginscreens
     echo $themePath
-    #git clone https://github.com/dgudim/themes &>> /dev/null
+    git clone https://github.com/dgudim/themes
 
     if isUserInstall "$installType"
     then 
-        echo "Splash Theme List"
-        printValidSplashThemes $themePath
-        local numThemes=$(numValidSplashThemes "$themePath")
-        echo ""
-        echo "Enter the theme to be installed alongside SpottyKDE."
-        read -rp "Enter num: (1-$numThemes) " themeSelection
-        while ! isValidInputNum $themeSelection 1 $numThemes
-        do
-            read -rp "Enter num: (1-$numThemes) " themeSelection
-        done
-        echo $themeSelection
-    fi
-
-}
-
-addSplashThemesAsGlobalThemes() {
-    local localDir=$1
-    local themeList
-    themeList=$(ls -d "$localDir"/themes/KDE-loginscreens/*/)
-    local theme
-    echo "Adding All Splash Screens As Global Themes"
-    for theme in $themeList
-    do 
-        if isGlobalTheme $theme
-        then
-            echo "Adding $(basename $theme)"
-            cp -r $theme "$HOME"/.local/share/plasma/look-and-feel
-        fi
-    done
-    echo "Splashes Added"
-}
-
-
-findSplashTheme() {
-    local num=$1
-    local themesDir=$2
-    themeDir=$(removeEndingSlash "$themesDir")
-    local themeList
-    themeList=$(ls -d "$themesDir"/*/)
-    local count=1
-
-    for theme in $themeList
-    do
-
-        if isGlobalTheme "$themesDir"/"$(basename "$theme")" && [ $count -eq "$num" ]
-        then
-            echo "$(basename "$theme")"
-            break
-        fi
-
-    done
-}
-numValidSplashThemes() {
-    local themesDir=$1
-    local themeList
-    themeList=$(ls -d "$themesDir"/*/)
-    local count=1
-    local theme
-
-    for theme in $themeList
-    do
+        local installDir="$HOME"/.local/share/plasma/look-and-feel/SpottyKDE/contents
         
-        if isGlobalTheme $theme
-        then
-            ((count++))
-        fi
-    done
-    echo $((count-1))
-}
-printValidSplashThemes() {
-    local themesDir=$1
-    local themeList
-    themeList=$(ls -d "$themesDir"/*/)
-    local count=1
-    local theme
+        addSplashsToDirectory $themePath $installDir
+    elif isSystemInstall "$installType"
+    then
+        local installDir=/usr/share/plasma/look-and-feel/SpottyKDE/contentsS
+        addSplashsToDirectory $themePath $installDir
+    else
+        local exitCode=144
+        echo "Invalid option or no option provided in function call"
+        echo "Exit Code: $exitCode"
+        exit $exitCode
+    fi
+    rm -rf $localDir/themes
+    echo "Install complete"
 
-    for theme in $themeList
-    do
-        if isGlobalTheme "$theme"
-        then
-            echo "$count: $(basename "$theme")"
-            ((count++))
-        fi
-    done
 }
 
 path=$(pwd)
+#printValidSplashThemes $path/themes/KDE-loginscreens
 installSplashScreens $path --user
